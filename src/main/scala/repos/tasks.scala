a7bc9d937ea5a4b133ad2363c6648ea4cabe75c5
MugishaU <muwiragiye123@gmail.com>
2020-11-27 16:35:26 +0000

    First Commit- Scala To-do list example

---
+++ src/main/scala/repos/tasks.scala
@@ -0,0 +1,9 @@
+package repos
+import domain.Task
+import scala.concurrent.Future
+
+trait TaskRepo {
+  def save (id:String, userId:String, title:String, done:Boolean): Future[Task]
+  def update(task: Task): Future[Task]
+  def get (id: String): Future[Option[Task]] //Distinguish between not found & other errors
+}
